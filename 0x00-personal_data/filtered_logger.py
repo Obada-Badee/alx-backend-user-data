58fbe6fd776010a76d67283cdde46ee439ce9383
Obada-Badee <obadabadee3@hotmail.com>
2024-08-16 01:31:03 +0300

    Tasks 3 and 4

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,10 +1,52 @@
 #!/usr/bin/env python3
-"""Personal data and logging module"""
-from typing import List
+"""
+A module that defines a function filter_datum
+"""
 import re
+import os
 import logging
+import mysql.connector
+from typing import List
+
 
 PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
+
+
+def get_logger() -> logging.Logger:
+    """Create a logging.Logger object"""
+    logger = logging.getLogger("user_data")
+    logger.propagate = False
+    logger.setLevel(logging.INFO)
+
+    sh = logging.StreamHandler()
+    sh.setFormatter(RedactingFormatter(list(PII_FIELDS)))
+    logger.addHandler(sh)
+
+    return logger
+
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """Obfuscate fileds inside a message"""
+    field = '|'.join(fields)
+    return re.sub(fr'({field})=[^{separator}]*', fr'\1={redaction}', message)
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """Creates a connector to a database.
+    """
+    db_host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
+    db_name = os.getenv("PERSONAL_DATA_DB_NAME", "")
+    db_user = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
+    db_pwd = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
+    connection = mysql.connector.connect(
+        host=db_host,
+        port=3306,
+        user=db_user,
+        password=db_pwd,
+        database=db_name,
+    )
+    return connection
 
 
 class RedactingFormatter(logging.Formatter):
@@ -26,21 +68,23 @@ class RedactingFormatter(logging.Formatter):
         return super(RedactingFormatter, self).format(record)
 
 
-def filter_datum(fields: List[str], redaction: str,
-                 message: str, separator: str) -> str:
-    """ Obfuscate fileds inside a message """
-    field = '|'.join(fields)
-    return re.sub(fr'({field})=[^{separator}]*', fr'\1={redaction}', message)
+def main() -> None:
+    """Obtain a database connection and retrieve all rows in the users table"""
+    fields = "name,email,phone,ssn,password,ip,last_login,user_agent"
+    cols = fields.split(',')
+    logger = get_logger()
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute(f"SELECT {fields} FROM users;")
+    for row in cursor:
+        msg = []
+        for idx, col in enumerate(cols):
+            msg.append(f'{col}={row[idx]}')
+        new = ';'.join(msg)
+        logger.info(new)
+    cursor.close()
+    db.close()
 
 
-def get_logger() -> logging.Logger:
-    """Create a logging.Logger object"""
-    logger = logging.getLogger("user_data")
-    logger.propagate = False
-    logger.setLevel(logging.INFO)
-
-    sh = logging.StreamHandler()
-    sh.setFormatter(RedactingFormatter(list(PII_FIELDS)))
-    logger.addHandler(sh)
-
-    return logger
+if __name__ == '__main__':
+    main()
